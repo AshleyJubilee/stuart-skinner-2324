1edbf31c8b86e75e7b92d4f1045389ccef4a2e3a
AshleyJubilee <ashleybjubilee@gmail.com>
2024-02-09 00:03:16 -0700

    team colour scheme and leauge wide analysis

---
+++ nhl_themes.py
@@ -0,0 +1,39 @@
+import plotly.graph_objects as go
+import plotly.io as pio
+
+teamColours = {
+'ANA': {'colour': '#F47A38', 'text': '#ffffff'},
+'ARI': {'colour': '#794848', 'text': '#ffffff'},
+'BOS': {'colour': '#FFB81C', 'text': '#000000'},
+'BUF': {'colour': '#003087', 'text': '#ffffff'},
+'CGY': {'colour': '#d6300e', 'text': '#ffffff'},
+'CAR': {'colour': '#CE1126', 'text': '#ffffff'},
+'CHI': {'colour': '#af2c1b', 'text': '#ffffff'},
+'COL': {'colour': '#6F263D', 'text': '#ffffff'},
+'CBJ': {'colour': '#002654', 'text': '#ffffff'},
+'DAL': {'colour': '#006847', 'text': '#ffffff'},
+'DET': {'colour': '#d52b3f', 'text': '#ffffff'},
+'EDM': {'colour': '#FF4C00', 'text': '#ffffff'},
+'FLA': {'colour': '#C8102E', 'text': '#ffffff'},
+'L.A': {'colour': '#111111', 'text': '#ffffff'},
+'MIN': {'colour': '#154734', 'text': '#ffffff'},
+'MTL': {'colour': '#AF1E2D', 'text': '#ffffff'},
+'NSH': {'colour': '#FFB81C', 'text': '#000000'},
+'N.J': {'colour': '#CE1126', 'text': '#ffffff'},
+'NYI': {'colour': '#F47D30', 'text': '#ffffff'},
+'NYR': {'colour': '#0038A8', 'text': '#ffffff'},
+'OTT': {'colour': '#b12323', 'text': '#ffffff'},
+'PHI': {'colour': '#F74902', 'text': '#ffffff'},
+'PIT': {'colour': '#FCB514', 'text': '#000000'},
+'STL': {'colour': '#041E42', 'text': '#ffffff'},
+'S.J': {'colour': '#006D75', 'text': '#ffffff'},
+'SEA': {'colour': '#001628', 'text': '#ffffff'},
+'T.B': {'colour': '#002868', 'text': '#ffffff'},
+'TOR': {'colour': '#00205B', 'text': '#ffffff'},
+'VAN': {'colour': '#173d83', 'text': '#ffffff'},
+'VGK': {'colour': '#B4975A', 'text': '#000000'},
+'WSH': {'colour': '#C8102E', 'text': '#ffffff'},
+'WPG': {'colour': '#041E42', 'text': '#ffffff'}}
+
+
+
